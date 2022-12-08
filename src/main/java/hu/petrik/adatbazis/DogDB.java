71ed772bd938fb7e07f6877d5bedc34ffb8f77a6
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-12-08 09:22:54 +0100

    Add Create and Read function

--- src/main/java/hu/petrik/adatbazis/DogDB.java
+++ src/main/java/hu/petrik/adatbazis/DogDB.java
@@ -1,25 +1,46 @@
 package hu.petrik.adatbazis;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DogDB {
     Connection conn;
     public static String DB_DRIVER = "mysql";
+    public static String DB_PORT = "3306";
+    public static String DB_HOST = "localhost";
+    public static String DB_DBNAME = "java";
     public static String DB_USER = "root";
     public static String DB_PASS = "";
 
-    public DogDB() {
-        String url = String.format("");
+    public DogDB() throws SQLException {
+        String url = String.format("jdbc:%s//%s:%s/%s", DB_DRIVER,DB_HOST,DB_PORT,DB_DBNAME);
         conn = DriverManager.getConnection(url ,DB_USER, DB_PASS);
     }
 
-    public void createDog() {
-
+    public boolean createDog(Dog dog) throws SQLException {
+        String sql = "INSERT INTO dogs(name, age , breed) VALUES (?,?,?)";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, dog.getName());
+        stmt.setInt(2, dog.getAge());
+        stmt.setString(3, dog.getBreed());
+        return stmt.executeUpdate() > 0;
     }
 
-    public void readDogs() {
-
+    public List<Dog> readDogs() throws SQLException {
+        List<Dog> dogs = new ArrayList<>();
+        String sql = "SELECT * FROM dogs";
+        Statement stmt = conn.createStatement();
+        ResultSet result = stmt.executeQuery(sql);
+        while (result.next()) {
+            int id = result.getInt("id");
+            String name = result.getString("name");
+            int age = result.getInt("age");
+            String breed = result.getString("breed");
+            Dog dog = new Dog(id, name, age, breed);
+            dogs.add(dog);
+        }
+        return dogs;
     }
 
     public void updateDogs() {
